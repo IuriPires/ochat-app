8ad6e563115884a1ebbb85d340843926c5679bcf
iuripires <iuripires.work@gmail.com>
2025-11-15 16:01:57 +0000

    adding main logo

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -2,14 +2,23 @@ import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { Button } from "@/components/ui/button";
 import { AppleSignInButton } from "@/components/ui/sign-in-with-apple";
+import { Image } from "expo-image";
 import { useRouter } from "expo-router";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Page() {
   const router = useRouter();
 
   return (
     <ThemedView className="flex-1 bg-black" lightColor="#000" darkColor="#000">
+      <View className="flex-1 items-center justify-center">
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={{ width: 200, height: 200 }}
+          contentFit="contain"
+        />
+      </View>
+
       <ThemedView
         className="flex-1 bg-black justify-end gap-4"
         lightColor="#000"
